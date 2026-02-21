22c2921105cffe848c1f1020fad626f243285b2a
druiz35 <escobarruizd@gmail.com>
2026-02-20 19:19:05 -0500

    Initial commit

---
+++ src/main/java/com/example/mockbankaccountservice/MockBankAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.mockbankaccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockBankAccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockBankAccountServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mockbankaccountservice/MockBankAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockbankaccountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockBankAccountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
